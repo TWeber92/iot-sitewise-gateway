507ee72aea0166a4e1276739960f8ff59da0015e
Tad Weber <tadweber92@gmail.com>
2026-09-21 12:49:58 -0700

    Rewrite get_hierarchy_properties with recursive tree builder

--- src/models/line_hierarchy.py
+++ src/models/line_hierarchy.py
@@ -1,13 +1,7 @@
 from enum import Enum
 
+
 class Hierarchy(Enum):
-    PARENT = 1
-    CHILD1 = 2
-    CHILD2 = 3
-
-
-completed_request_body = {
-    'PARENT': Hierarchy.PARENT,
-    'CHILD1': Hierarchy.CHILD1,
-    'CHILD2': Hierarchy.CHILD2
-}
+    PARENT = 0
+    CHILD1 = 1
+    CHILD2 = 2
